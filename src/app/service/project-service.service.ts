ea2a272da25f992cb38387781bfa4c6a2cd328ea
NicoButter <nicobutter@gmail.com>
2023-04-02 13:26:17 -0300

    cambios_autorizados

--- src/app/service/project-service.service.ts
+++ src/app/service/project-service.service.ts
@@ -10,30 +10,30 @@ export class ProjectServiceService {
 
   //URL = environment.URL + 'projects/';
   
-  URL = 'https://portfolio-bkd-nicolas-butterfield.onrender.com/projects/';
+  URL = 'https://portfolio-bkd-nicolas-butterfield.onrender.com/projects';
 
   //URL = 'http://localhost:8080/projects/';
 
   constructor(private httpClient: HttpClient) { }
 
   public lista():Observable<Projects[]>{
-    return this.httpClient.get<Projects[]>(this.URL + 'lista');
+    return this.httpClient.get<Projects[]>(this.URL + '/lista');
   }
 
   public detail(id: number): Observable<Projects>{
-    return this.httpClient.get<Projects>(this.URL + `detail/${id}`);
+    return this.httpClient.get<Projects>(this.URL + `/detail/${id}`);
   }
 
   public save(project: Projects): Observable<any>{
-    return this.httpClient.post<any>(this.URL + 'create', project);
+    return this.httpClient.post<any>(this.URL + '/create', project);
   }
 
   public update (id: number, project: Projects): Observable<any>{
-    return this.httpClient.put<any>(this.URL + `update/${id}`, project);
+    return this.httpClient.put<any>(this.URL + `/update/${id}`, project);
   }
 
   public delete(id: number): Observable<any>{
-    return this.httpClient.delete<any>(this.URL + `delete/${id}`);
+    return this.httpClient.delete<any>(this.URL + `/delete/${id}`);
   }
 
 }
