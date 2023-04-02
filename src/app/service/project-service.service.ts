478d22899c9179cd59d8561c53d5fcc1b4ff416a
NicoButter <nicobutter@gmail.com>
2023-04-02 18:18:00 -0300

    correcciones_permitidas

--- src/app/service/project-service.service.ts
+++ src/app/service/project-service.service.ts
@@ -2,15 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Projects } from '../model/projects';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectServiceService {
 
-  //URL = environment.URL + 'projects/';
+  URL = environment.URL + 'projects/';
   
-  URL = 'https://portfolio-bkd-nicolas-butterfield.onrender.com/projects/';
+  //URL = 'https://portfolio-bkd-nicolas-butterfield.onrender.com/projects/';
 
   //URL = 'http://localhost:8080/projects/';
 
